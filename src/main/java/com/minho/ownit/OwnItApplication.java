d79c6364f4f351327c8244d7c0972c5c1fdce73e
coghcogh0411 <coghcogh0411@naver.com>
2025-01-14 21:22:52 +0900

    homeController

--- src/main/java/com/minho/ownit/OwnItApplication.java
+++ src/main/java/com/minho/ownit/OwnItApplication.java
@@ -2,8 +2,9 @@ package com.minho.ownit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class OwnItApplication {
 
 	public static void main(String[] args) {
