3e7a3726325aea5597e3416829264b6cf8b9bd2e
coghcogh0411 <coghcogh0411@naver.com>
2025-01-15 22:03:16 +0900

    index

--- src/main/java/com/minho/ownit/HomeController.java
+++ src/main/java/com/minho/ownit/HomeController.java
@@ -3,11 +3,14 @@ package com.minho.ownit;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class HomeController {
 	@GetMapping("/")
-	public String home() {
+	public String home(HttpServletRequest req) {
+		req.setAttribute("contentPage", "home");
 		return "index";
 	}
 	
