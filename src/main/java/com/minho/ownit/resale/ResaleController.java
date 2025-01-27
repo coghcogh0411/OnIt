1c9b01262bd0b3bb23986ae093cef444026cd9d6
coghcogh0411 <coghcogh0411@naver.com>
2025-01-28 03:28:03 +0900

    resaleReg

---
+++ src/main/java/com/minho/ownit/resale/ResaleController.java
@@ -0,0 +1,23 @@
+package com.minho.ownit.resale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class ResaleController {
+	@GetMapping("/resale")
+	public String resaleHome(HttpServletRequest req) {
+		req.setAttribute("contentPage", "resale/resalehome");
+		return "index";
+	}
+	
+	@GetMapping("/resale-reg")
+	public String ResaleReg(HttpServletRequest req) {		
+		req.setAttribute("contentPage", "resale/resalereg");
+		return "index";
+	}
+	
+}
