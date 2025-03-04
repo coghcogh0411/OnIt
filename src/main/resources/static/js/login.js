f1ddac28395562e9eb9636af5c20b574123e430d
coghcogh0411 <coghcogh0411@naver.com>
2025-03-04 22:15:03 +0900

    로그인표시

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,20 +1,3 @@
-$(function() {
-	var user = document.getElementById("user");
-	const modalOverlay = document.querySelector(".modal-overlay");
-	var mypage = document.getElementById("myPage");
-	
-	if (user == null) {
-		mypage.addEventListener("click", (e) => {
-			modalOverlay.style.display = "flex"; // 보이게
-			
-		});
-		// 모달 바깥 클릭 시 닫기 (선택사항)
-		modalOverlay.addEventListener("click", (e) => {
-			if (e.target === modalOverlay) {
-				modalOverlay.style.display = "none";
-			}
-		});
-
-	}
-
-})
+function openLoginModal() {
+    document.querySelector('.modal-overlay').style.display = 'flex';
+  }
